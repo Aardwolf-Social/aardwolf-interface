30744e7aaac9c6eb2f9c7143a2bade78374e4f42
Banjo Fox <drbanjofox@protonmail.com>
2023-06-29 19:30:34 -0400

    Removing yew-app target

--- aardwolf-yew-app/target/debug/deps/aardwolf_yew_app-23e722ef94b01776.d
+++
@@ -1,5 +0,0 @@
-/home/quixote/gits/aardwolf-interface/aardwolf-yew-app/target/debug/deps/aardwolf_yew_app-23e722ef94b01776.rmeta: src/main.rs
-
-/home/quixote/gits/aardwolf-interface/aardwolf-yew-app/target/debug/deps/aardwolf_yew_app-23e722ef94b01776.d: src/main.rs
-
-src/main.rs:
--- aardwolf-yew-app/target/debug/deps/aardwolf_yew_app-97a5b892262dea3e.d
+++
@@ -1,5 +0,0 @@
-/home/quixote/gits/aardwolf-interface/aardwolf-yew-app/target/debug/deps/aardwolf_yew_app-97a5b892262dea3e.rmeta: src/main.rs
-
-/home/quixote/gits/aardwolf-interface/aardwolf-yew-app/target/debug/deps/aardwolf_yew_app-97a5b892262dea3e.d: src/main.rs
-
-src/main.rs:
