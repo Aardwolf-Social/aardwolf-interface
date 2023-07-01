713380bda96920614fd078335d93cc553b68529c
Banjo Fox <drbanjofox@protonmail.com>
2023-07-01 17:21:28 -0400

    Adding crate:ybc for Bulma functionality

--- aardwolf-yew-app/src/lib.rs
+++ aardwolf-yew-app/src/lib.rs
@@ -1,6 +1,11 @@
 use yew::prelude::*;
 use serde::{Serialize, Deserialize};
 
+// ybc crate imports -- https://crates.io/crates/ybc
+use ybc::NavbarFixed::Top;
+use ybc::TileCtx::{Ancestor, Child, Parent};
+use ybc::TileSize::Four;
+
 // Lets make a struct for testing console logging
 #[derive(Serialize, Deserialize)] 
 struct LogMe {
@@ -33,18 +38,81 @@ pub fn aardwolf() -> Html {
     // Vector used to build an HTML list
     let tasks = vec!["item-1", "item-2", "item-3"];
 
-
-
     // Start of the html! Yew macro
     html!{
         <>
-            <h1>{"Aardwolf Yew Genesis"}</h1>
-            <p>{"This page is being rendered by Yew $version, and powered by Trunk!"}</p>
+        <ybc::Navbar
+            classes={classes!("is-success")}
+            padded=true
+            navbrand={html!{
+                <ybc::NavbarItem>
+                    <ybc::Title classes={classes!("has-text-white")} size={ybc::HeaderSize::Is4}>{"Trunk | Yew | YBC"}</ybc::Title>
+                </ybc::NavbarItem>
+            }}
+            navstart={html!{}}
+            navend={html!{
+                <>
+                <ybc::NavbarItem>
+                    <ybc::ButtonAnchor classes={classes!("is-black", "is-outlined")} rel={String::from("noopener noreferrer")} target={String::from("_blank")} href="https://github.com/thedodd/trunk">
+                        {"Trunk"}
+                    </ybc::ButtonAnchor>
+                </ybc::NavbarItem>
+                <ybc::NavbarItem>
+                    <ybc::ButtonAnchor classes={classes!("is-black", "is-outlined")} rel={String::from("noopener noreferrer")} target={String::from("_blank")} href="https://yew.rs">
+                        {"Yew"}
+                    </ybc::ButtonAnchor>
+                </ybc::NavbarItem>
+                <ybc::NavbarItem>
+                    <ybc::ButtonAnchor classes={classes!("is-black", "is-outlined")} rel={String::from("noopener noreferrer")} target={String::from("_blank")} href="https://github.com/thedodd/ybc">
+                        {"YBC"}
+                    </ybc::ButtonAnchor>
+                </ybc::NavbarItem>
+                </>
+            }}
+        /> // End of ybc::Navbar
 
-            <ul>
-                {list_to_html(tasks)}
-            </ul>
-
+        <ybc::Hero
+            classes={classes!("is-light")}
+            size={ybc::HeroSize::FullheightWithNavbar}
+            body={html!{
+                <ybc::Container classes={classes!("is-centered")}>
+                <ybc::Tile ctx={Ancestor}>
+                    <ybc::Tile ctx={Parent} size={ybc::TileSize::Twelve}>
+                        <ybc::Tile ctx={Parent}>
+                            <ybc::Tile ctx={Child} classes={classes!("notification", "is-success")}>
+                                <ybc::Subtitle size={ybc::HeaderSize::Is3} classes={classes!("has-text-white")}>{"Trunk"}</ybc::Subtitle>
+                                <p>{"Trunk is a WASM web application bundler for Rust."}</p>
+                            </ybc::Tile>
+                        </ybc::Tile>
+                        <ybc::Tile ctx={Parent}>
+                            <ybc::Tile ctx={Child} classes={classes!("notification", "is-success")}>
+                                <ybc::Icon size={ybc::Size::Large} classes={classes!("is-pulled-right")}><img src="yew.svg"/></ybc::Icon>
+                                <ybc::Subtitle size={ybc::HeaderSize::Is3} classes={classes!("has-text-white")}>
+                                    {"Yew"}
+                                </ybc::Subtitle>
+                                <p>{"Yew is a modern Rust framework for creating multi-threaded front-end web apps with WebAssembly."}</p>
+                            </ybc::Tile>
+                        </ybc::Tile>
+                        <ybc::Tile ctx={Parent}>
+                            <ybc::Tile ctx={Child} classes={classes!("notification", "is-success")}>
+                                <ybc::Subtitle size={ybc::HeaderSize::Is3} classes={classes!("has-text-white")}>{"YBC"}</ybc::Subtitle>
+                                <p>{"A Yew component library based on the Bulma CSS framework."}</p>
+                            </ybc::Tile>
+                        </ybc::Tile>
+                        <ybc::Tile ctx={Parent}>
+                            <ybc::Tile ctx={Child} classes={classes!("notification", "is-success")}>
+                                <ybc::Subtitle size={ybc::HeaderSize::Is3} classes={classes!("has-text-white")}>{"Function Test"}</ybc::Subtitle>
+                                <p>{"This is a list generated from a vec!"}</p>
+                                <ul>
+                                    {list_to_html(tasks)}
+                                </ul>
+                            </ybc::Tile>
+                        </ybc::Tile>                        
+                    </ybc::Tile>
+                </ybc::Tile>
+                </ybc::Container>
+            }}>
+        </ybc::Hero>
         </>
     }
 
@@ -52,7 +120,7 @@ pub fn aardwolf() -> Html {
 
 
 // Function to convert lists into HTML
-fn list_to_html(list: Vec<&str>) -> Vec<Html> {
+pub fn list_to_html(list: Vec<&str>) -> Vec<Html> {
     // Iterate items onto a map, wrap them in <li> tags, and build the new list
     list.iter().map(|item| html!{<li>{item}</li>}).collect()
 }
